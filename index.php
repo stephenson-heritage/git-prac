9ba809aee9349039f2c73454f74e6cb4502d47e6
stephenson-heritage <LStephenson@cegep-heritage.qc.ca>
2021-03-03 08:43:19 -0500

    removed template

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once("inc/config/config.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "Template - " . $page_data["title"] ?></title>
+    <title><?php echo $page_data["title"] ?></title>
     <link rel="stylesheet" href="css/style.css" />
 </head>
 
