2974ed2282a9957437221baf8d248810aabb1e0f
stephenson-heritage <LStephenson@cegep-heritage.qc.ca>
2021-03-03 09:45:37 -0500

    old index, much more minimalistic

--- index.php
+++ index.php
@@ -1,45 +1,4 @@
 <?php
 
-$config_path = getcwd();
-require_once("inc/config/config.php");
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $page_data["title"] ?></title>
-    <link rel="stylesheet" href="css/style.css" />
-</head>
-
-<body>
-    body - test
-    <?php
-
-
-    $mainMenu = new Menu($page, "main");
-    $mainMenu->dbLoad($db);
-    echo $mainMenu;
-
-    User::view(
-        $logged_in,
-        $create_account,
-        isset($config_user["data"]) ? $config_user["data"] : []
-    );
-
-    // include the page we are on
-    include("pages/" . $page_data["script"]);
-
-
-    $footerMenu = new Menu($page, "footer");
-    $footerMenu->dbLoad($db);
-    echo $footerMenu;
-
-    ?>
-    p3 change
-</body>
-
-</html>
+echo "helloe world v2";
